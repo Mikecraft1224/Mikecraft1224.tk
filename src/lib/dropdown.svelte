246e0d53bd87524423626b29a62b85ea2f06a089
Mikecraft1224 <Mikecraft1224@gmail.com>
2022-02-26 20:15:31 +0100

    changed navbar icons

--- src/lib/dropdown.svelte
+++ src/lib/dropdown.svelte
@@ -10,8 +10,15 @@
 
 <div class="float-left overflow-hidden text-mainhf" on:mouseleave={() => {if (!visible) {hover = false}; visible = false; hovering = false}} on:mouseenter={() => {hover = true; hovering = true}}>
   <button class="{hover == true ? "bg-hoverhb text-hoverhf" : ""} dropbtn cursor-pointer border-none outline-none px-4 py-1 rounded-md flex items-center m-0 transition duration-300 ease-in-out" on:click={() => {visible = !visible}}>
-    <div class="hidden md:flex md:items-center md:justify-center"><slot name="img"/></div>
-    <div class="hidden md:flex md:pl-1">&#8192;|&#8192;</div> <div>{name}</div>
+    <div class="hidden md:flex md:items-center md:justify-center md:w-5">
+      {#if !visible}
+        <slot name="img1"/>
+      {:else}
+      <slot name="img2"/>
+      {/if}
+    </div>
+    <div class="hidden md:flex">&#8192;|&#8192;</div> 
+    <div>{name}</div>
   </button>
   {#if visible}
   <div class="absolute bg-drophb min-w-[200px] shadow-md z-[1] rounded-md" transition:slide={{ duration: 300, easing: sineInOut }} on:outroend={() => {if (!hovering) {hover = false}}}>
--- src/lib/navbar_entry.svelte
+++ src/lib/navbar_entry.svelte
@@ -5,5 +5,6 @@
 
 <a href={path} class="{$page.url.pathname == path ? "bg-mainhb text-hoverhf group-hover:bg-hoverhb group-hover:text-mainhf" : "text-mainhf"} px-4 py-1 rounded-md flex items-center  hover:bg-hoverhb hover:text-hoverhf transition duration-300 ease-out">
   <div class="hidden md:flex md:items-center md:justify-center"><slot name="img"/></div>
-  <div class="hidden md:flex md:pl-1">&#8192;|&#8192;</div> <div>{name}</div>
+  <div class="hidden md:flex">&#8192;|&#8192;</div> 
+  <div>{name}</div>
 </a>
