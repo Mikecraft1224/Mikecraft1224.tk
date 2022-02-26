4e10292ab70973046a5dbd054511a7b33be899ba
Mikecraft1224 <Mikecraft1224@gmail.com>
2022-02-26 23:02:25 +0100

    fix

--- src/lib/navbar_entry.svelte
+++ src/lib/navbar_entry.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <a href={path} class="{$page.url.pathname == path ? "bg-mainhb text-hoverhf group-hover:bg-hoverhb group-hover:text-mainhf" : "text-mainhf hover:bg-hoverhb hover:text-hoverhf "} px-4 py-1 rounded-md flex items-center transition duration-300 ease-out">
-  <div class="text-current hidden md:flex md:items-center md:justify-center">a<slot name="img"/></div>
+  <div class="text-current hidden md:flex md:items-center md:justify-center"><slot name="img"/></div>
   <div class="text-current hidden md:flex">&#8192;|&#8192;</div> 
   <div class="text-current">{name}</div>
 </a>
